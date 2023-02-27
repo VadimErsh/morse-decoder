a4e22115175e2bfc124e8bb8c12a3990dd3c26a6
VadimErsh <vadimersh2022@gmail.com>
2023-02-27 23:32:44 +0300

    end test

--- src/index.js
+++ src/index.js
@@ -38,7 +38,59 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrExpr = expr.split('')
+  let updateArrExpr = []
+  let morseArr = []
+  let res = []
+
+  for(let i = 0; i < expr.length; i++) {
+    updateArrExpr.push(arrExpr.slice(0, 10))
+    arrExpr = arrExpr.splice(10, arrExpr.length)
+  }
+
+  for(let i = 0; i < updateArrExpr.length; i++) {
+    for(let j = 0; j < updateArrExpr[i].length; j++) {
+      if(updateArrExpr[i][j] === '0') {
+        updateArrExpr[i].shift()
+        j--
+      }
+      if(updateArrExpr[i][j] === '1' && updateArrExpr[i][j + 1] === '0') {
+        morseArr.push('.')
+        updateArrExpr[i].shift()
+        updateArrExpr[i].shift()
+        j -= 2
+
+      }
+      if(updateArrExpr[i][j] === '1' && updateArrExpr[i][j + 1] === '1') {
+        morseArr.push('-')
+        updateArrExpr[i].shift()
+        updateArrExpr[i].shift()
+        j -= 2
+      }
+      if(updateArrExpr[i][j] === '*') {
+        morseArr.push(' ')
+        updateArrExpr[i] = []
+      }
+    }
+    morseArr.push(';')
+  }
+
+  morseArr = morseArr.join('').split(';')
+  morseArr.pop()
+
+
+  morseArr.forEach(el => {
+    if(el === ' ') {
+      res.push(' ')
+    }
+    for (let key in MORSE_TABLE) {
+      if(el === key) {
+        res.push(MORSE_TABLE[key])
+      }
+    }
+  })
+
+  return res.join('')
 }
 
 module.exports = {
